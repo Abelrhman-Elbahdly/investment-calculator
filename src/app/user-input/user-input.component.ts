0082d3cb5ee9932b214a810d8ad7be5df05a869b
Abdelrhman Moustafa Elbahdly <a.elbahdly@gmail.com>
2025-04-08 22:09:51 +0200

    Initial commit

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,29 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentService } from '../investment.service';
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+  constructor(private invesmentService: InvestmentService) {}
+  onSubmit() {
+    this.invesmentService.CalculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+      expectedReturn: +this.enteredExpectedReturn(),
+    });
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
+  }
+}
